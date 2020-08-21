bd2a627522ece8a35ad8f84e832511aa7e8db27f
Clarence Chan <38497249+clarencecodes@users.noreply.github.com>
2020-08-22 00:17:26 +0800

    Initial Commit

---
+++ SIAAppChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SIAAppChallenge
+//
+//  Created by Clarence Chan on 22/8/20.
+//  Copyright © 2020 High Flyers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
